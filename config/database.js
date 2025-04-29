c9eb535a05f33849b9d745b770d6ec4e7b40a68c
medinag-codes <medinageyer@gmail.com>
2025-04-29 13:34:26 -0400

    First commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+    url : 'mongodb+srv://medinageyer:demo@fullstack.qefrkm5.mongodb.net/familyRecipes?retryWrites=true&w=majority&appName=fullStack',
+    'dbName': 'familyRecipes'
+};
